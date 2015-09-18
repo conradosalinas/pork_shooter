97cab64e058cb9ef3931fd21de32b5107bf8cc82
viobearcs@gmail.com <viobearcs@gmail.com>
2015-09-18 13:10:37 -0700

    added project

---
+++ PopThePorks_edge.js
@@ -0,0 +1,35 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x10='nowrap',e26='${piggyCopy9}',x11='rgba(0,0,0,0)',g='image',e19='${piggyCopy7}',e22='${piggyCopy5}',e28='${piggyCopy4}',x='text',e25='${piggyCopy8}',m='rect',e18='${Text2}',e27='${piggyCopy3}',i='none',x14='42',x3='6.0.0.400',p='px',o='opacity',e21='${piggy}',x15='Arial, Helvetica, sans-serif',l='normal',x5='rgba(225,66,66,1.00)',e24='${piggyCopy}',e23='${piggyCopy10}',x9='break-word',x17='rgba(255,255,255,1)',x16='600',x7='24',x8='bangers, sans-serif',x2='5.0.0',xc='rgba(0,0,0,1)',x4='both',e20='${piggyCopy2}',tp='top';var g12='piggy.svg';var s13="<p style=\"margin: 0px;\">​RESTART</p>",s6="<p style=\"margin: 0px;\">​All piggies must die!</p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={'bangers, sans-serif':'<script src=\"http://use.edgefonts.net/bangers:n4:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:x4,rI:n,cn:{dom:[{id:'Rectangle',t:m,r:['-27px','-11px','595px','421px','auto','auto'],cu:'crosshair',f:[x5],s:[0,xc,i]},{id:'Text',t:x,r:['188px','111px','auto','auto','auto','auto'],cu:'crosshair',text:s6,n:[x8,[x7,],"rgba(0,0,0,1.00)",l,i,"",x9,x10]},{id:'piggyCopy10',t:g,r:['134px','-230px','312px','283px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggy',t:g,r:['420px','-115px','130px','129px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy',t:g,r:['179px','-169px','222px','200px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy2',t:g,r:['331px','-103px','102px','92px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy3',t:g,r:['68px','-120px','143px','129px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy5',t:g,r:['-10px','-148px','181px','164px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy4',t:g,r:['0px','-62px','75px','68px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy7',t:g,r:['30px','-144px','181px','164px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy8',t:g,r:['155px','-74px','102px','92px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'piggyCopy9',t:g,r:['347px','-137px','181px','164px','auto','auto'],cu:'crosshair',f:[x11,im+g12,'0px','0px']},{id:'Text2',t:x,r:['169px','254px','211px','42px','auto','auto'],overflow:'visible',o:'0',text:s13,align:"center",n:[x15,[x14,p],"rgba(255,255,255,1.00)",x16,i,"",x9,""]}],style:{'${Stage}':{isStage:true,r:['null','null','550px','400px','auto','auto'],overflow:'hidden',f:[x17]}}},tt:{d:5563,a:y,data:[["eid30",o,0,5500,"linear",e18,'0.000000','0'],["eid31",o,5500,63,"linear",e18,'0','1'],["eid22",tp,3389,922,"linear",e19,'388px','160px'],["eid23",tp,4311,1035,"linear",e19,'160px','-144px'],["eid2",tp,0,3000,"linear",e20,'400px','-103px'],["eid13",tp,2188,2469,"linear",e21,'388px','-115px'],["eid15",tp,2291,2650,"linear",e22,'388px','-148px'],["eid25",tp,3500,1441,"linear",e23,'360px','-230px'],["eid4",tp,0,5196,"linear",e24,'415px','-169px'],["eid18",tp,2894,2264,"linear",e25,'395px','-85px'],["eid28",tp,5158,405,"linear",e25,'-85px','-74px'],["eid20",tp,2894,2669,"linear",e26,'400px','-137px'],["eid6",tp,545,3766,"linear",e27,'457px','-120px'],["eid8",tp,1792,1859,"linear",e28,'412px','-62px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-2189725");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${piggy}","click",function(sym,e){if(sym.$("piggy").is(":visible")){sym.$("piggy").hide();}else{sym.$("piggy").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy2}","click",function(sym,e){if(sym.$("piggyCopy2").is(":visible")){sym.$("piggyCopy2").hide();}else{sym.$("piggyCopy2").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy}","click",function(sym,e){if(sym.$("piggyCopy").is(":visible")){sym.$("piggyCopy").hide();}else{sym.$("piggyCopy").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy5}","click",function(sym,e){if(sym.$("piggyCopy5").is(":visible")){sym.$("piggyCopy5").hide();}else{sym.$("piggyCopy5").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy3}","click",function(sym,e){if(sym.$("piggyCopy3").is(":visible")){sym.$("piggyCopy3").hide();}else{sym.$("piggyCopy3").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy4}","click",function(sym,e){if(sym.$("piggyCopy4").is(":visible")){sym.$("piggyCopy4").hide();}else{sym.$("piggyCopy4").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy7}","click",function(sym,e){if(sym.$("piggyCopy7").is(":visible")){sym.$("piggyCopy7").hide();}else{sym.$("piggyCopy7").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy8}","click",function(sym,e){if(sym.$("piggyCopy8").is(":visible")){sym.$("piggyCopy8").hide();}else{sym.$("piggyCopy8").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy9}","click",function(sym,e){if(sym.$("piggyCopy9").is(":visible")){sym.$("piggyCopy9").hide();}else{sym.$("piggyCopy9").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${piggyCopy10}","click",function(sym,e){if(sym.$("piggyCopy10").is(":visible")){sym.$("piggyCopy10").hide();}else{sym.$("piggyCopy10").show();}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Text2}","click",function(sym,e){sym.play(0);sym.$("piggyCopy10").show();sym.$("piggy").show();sym.$("piggyCopy2").show();sym.$("piggyCopy").show();sym.$("piggyCopy9").show();sym.$("piggyCopy8").show();sym.$("piggyCopy7").show();sym.$("piggyCopy4").show();sym.$("piggyCopy3").show();sym.$("piggyCopy5").show();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.$("Text2").hide();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",5563,function(sym,e){sym.$("Text2").show();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-2189725");
